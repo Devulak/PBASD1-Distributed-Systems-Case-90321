8a35934a88666807e701d2eadf5849235ff65f3b
Devulak <Devulak@users.noreply.github.com>
2021-03-08 11:20:28 +0100

    Init

---
+++ Case 90321/Case 90321/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Case_90321.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Case 90321/DataModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
